170bcdc7f261e256a661efa44917fa15c99c55be
OmriBerger <omriber12@gmail.com>
2024-05-06 19:26:32 +0300

    added embed again

--- app/pages/support/layout.tsx
+++ app/pages/support/layout.tsx
@@ -1,5 +1,6 @@
 import { Open_Sans } from 'next/font/google';
 import '../../public/styles/support.css';
+import { Metadata } from 'next';
 
 
 const open_sans = Open_Sans({
@@ -7,6 +8,19 @@ const open_sans = Open_Sans({
     display: 'swap',
     subsets: ['hebrew']
 })
+
+export const metadata: Metadata = {
+  title: 'טופס בעיות מחשבים',
+  openGraph: {
+    title: 'טופס בעיות מחשבים',
+    siteName: "טופס בעיות מחשבים",
+    description: "טופס למילוי לבקשת עזרה בנוגע בעיות מחשבים",
+    url: "https://rabincomputerspatrol.github.io/pages/support",
+    images: "https://raw.githubusercontent.com/RabinComputersPatrol/rabincomputerspatrol.github.io/main/app/public/assets/rabin-logo-small.png",
+    locale: 'he_IL',
+    type: "website"
+  }
+}
 
 export default function SupportLayout({
     children,
--- app/pages/support/support.server.tsx
+++ app/pages/support/support.server.tsx
@@ -3,14 +3,6 @@ import Head from 'next/head';
 export default function SupportPage() {
   return (
     <div className="support-page">
-      <Head>
-        <title>טופס בעיות מחשבים</title>
-        <meta name="description" content="טופס למילוי לבקשת עזרה בנוגע בעיות מחשבים"></meta>
-        <meta property="og:title" content="טופס בעיות מחשבים"></meta>
-        <meta property="og:url" content="https://rabincomputerspatrol.github.io/support"></meta>
-        <meta property="og:image" content="https://raw.githubusercontent.com/RabinComputersPatrol/rabincomputerspatrol.github.io/d464054ac77e202cf2e7faf6dcadbf3c60137d07/public/assets/rabin-logo.png"></meta>
-      </Head>
-      
       <div className='title-container'>
         <h1 className="title">טופס בעיות מחשבים</h1>
         <img src="@/app/public/assets/rabin-logo.png" alt="Rabin Logo" className='logo-big'/>
