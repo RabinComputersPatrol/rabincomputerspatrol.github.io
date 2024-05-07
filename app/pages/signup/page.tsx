e284f75d8552327988811e85183210d114c0a18a
OmriBerger <omriber12@gmail.com>
2024-05-07 23:57:43 +0300

    some problem fixes

--- app/pages/signup/page.tsx
+++ app/pages/signup/page.tsx
@@ -1,6 +1,5 @@
 import SupportPage from './signup.server.';
 import SupportForm from './signup.client';
-import { Login } from './signup.client';
 
 export default function Support() {
   return (
@@ -9,7 +8,6 @@ export default function Support() {
       <SupportForm />
       <footer className="footer">
         © 2024 נועם ולט. כל הזכויות שמורות.
-        <Login/>
       </footer>
     </div>
   );
