afb0726852c5ad7cdb4d100125c40c1838388f53
OmriBerger <omriber12@gmail.com>
2024-05-07 19:13:53 +0300

    added automatic redirect to support page if the page is not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,14 @@
-"use client"
+"use server"
 
-export default function ERROR() {
+import {redirect} from 'next/navigation';
+
+export default async function ERROR() {
+    redirect("/pages/support")
+
     return (
         <div>
             <h1 dir="ltr">404 Page</h1>
         </div>
     );
 }
+
