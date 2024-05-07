8b8805a2ed5bb60e5ead70eac7b9517143666abe
OmriBerger <omriber12@gmail.com>
2024-05-07 18:12:13 +0300

    image optimization and small env change

--- app/pages/support/support.server.tsx
+++ app/pages/support/support.server.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head';
 import rabinLogoSmall from "../../public/assets/rabin-logo-small.png";
 import rabinLogo from "../../public/assets/rabin-logo.png";
+import Image from 'next/image';
 
 export default function SupportPage() {
   return (
     <div className="support-page">
       <div className='title-container'>
         <h1 className="title">טופס בעיות מחשבים</h1>
-        <img src={rabinLogo.src} alt="Rabin Logo" className='logo-big' />
-        <img src={rabinLogoSmall.src} alt="Rabin Logo Small" className='logo-small' />
+        <Image src={rabinLogo.src} alt="Rabin Logo" className='logo-big'/>
+        <Image src={rabinLogoSmall.src} alt="Rabin Logo Small" className='logo-small' />
       </div>
     </div>
   );
