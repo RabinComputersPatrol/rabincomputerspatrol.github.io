fb79a411969a0f3d5d5d73a6f1d4482f69028b99
DanPeled <danpeled31@gmail.com>
2024-05-07 21:34:45 +0300

    fixed styles for mobile

---
+++ app/pages/dashboard/dashboard.client.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import Head from "next/head";
+import React from "react";
+
+export default function DashboardPage() {
+    return (
+        <div className="dash">
+            <div className="center container"></div>
+        </div>
+    );
+}
---
+++ app/pages/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import DashboardPage from "./dashboard.client";
+export default function Dashbaord() {
+    return (
+        <div>
+            <DashboardPage />
+        </div>
+    );
+}
