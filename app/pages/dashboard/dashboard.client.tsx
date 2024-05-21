1b01c14a58f03d495d7829405fa9f67e51a4ae5d
DanPeled <danpeled31@gmail.com>
2024-05-21 15:34:58 +0300

    made it get the report data :D

--- app/pages/dashboard/dashboard.client.tsx
+++ app/pages/dashboard/dashboard.client.tsx
@@ -35,7 +35,7 @@ export default function DashboardPage() {
                     fixed?: number; // Changed from boolean to accept 4 possible values
                 }[] = await getAllDocuments("dummy");
 
-                const formattedData: IRowData[] = rawData.map(item => ({
+                let formattedData: IRowData[] = rawData.map(item => ({
                     id: item.id || "",
                     priority: item.priority || 0,
                     problemDescription: item.problemDescription || "",
@@ -43,17 +43,26 @@ export default function DashboardPage() {
                     roomNumber: item.roomNumber || 0,
                     date: item.date || "",
                     phoneNumber: item.phoneNumber || "",
-                    fixed: item.fixed || -1 // Set default value to -1
+                    fixed: item.fixed ?? -1 // Set default value to -1
                 }));
 
-                formattedData.sort((n1, n2) => {
-                    return parseInt(n1.id) - parseInt(n2.date);
+                console.log("Formatted data before sorting:", formattedData);
+
+                formattedData = formattedData.sort((n1, n2) => {
+                    if (n1.id > n2.id) {
+                        return 1;
+                    } else if (n1.id < n2.id) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
                 });
+
+                console.log("Formatted data after sorting by ID:", formattedData);
 
                 setTableData(formattedData);
             } catch (error) {
                 console.error("Error getting documents: ", error);
-                // Handle error if necessary
             }
         }
         fetchData();
@@ -99,9 +108,6 @@ export default function DashboardPage() {
                     bValue = new Date(bValue as string).getTime();
                 }
 
-                if (typeof aValue === 'boolean') aValue = aValue ? 1 : 0;
-                if (typeof bValue === 'boolean') bValue = bValue ? 1 : 0;
-
                 if (aValue < bValue) {
                     return direction === 'ascending' ? -1 : 1;
                 }
@@ -118,48 +124,49 @@ export default function DashboardPage() {
         if (showHighPriority && rowData.priority < 5) return false;
         return true;
     });
-
     function createTable() {
-        return (<div>
-            <table>
-                <thead>
-                    <tr>
-                        <th onClick={() => updateSortConfig('id', sortConfig.keys.find(k => k.key === 'id')?.direction === 'ascending' ? 'descending' : 'ascending')}>
-                            ID {sortConfig.keys.find(k => k.key === 'id')?.direction === 'ascending' ? '▼' : '▲'}
-                        </th>
-                        <th onClick={() => updateSortConfig('priority', sortConfig.keys.find(k => k.key === 'priority')?.direction === 'ascending' ? 'descending' : 'ascending')}>
-                            Priority {sortConfig.keys.find(k => k.key === 'priority')?.direction === 'ascending' ? '▼' : '▲'}
-                        </th>
-                        <th> Problem Description </th>
-                        <th> Name </th>
-                        <th> Room Number </th>
-                        <th onClick={() => updateSortConfig('date', sortConfig.keys.find(k => k.key === 'date')?.direction === 'ascending' ? 'descending' : 'ascending')}>
-                            Date {sortConfig.keys.find(k => k.key === 'date')?.direction === 'ascending' ? '▼' : '▲'}
-                        </th>
-                        <th> Phone Number </th>
-                        <th onClick={() => updateSortConfig('fixed', sortConfig.keys.find(k => k.key === 'fixed')?.direction === 'ascending' ? 'descending' : 'ascending')}>
-                            Fixed {sortConfig.keys.find(k => k.key === 'fixed')?.direction === 'ascending' ? '▼' : '▲'}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sortedTableData.map((rowData, index) => (
-                        <tr key={index}>
-                            <td>{rowData.id}</td>
-                            <td>{rowData.priority}</td>
-                            <td>{rowData.problemDescription}</td>
-                            <td>{rowData.name}</td>
-                            <td>{rowData.roomNumber}</td>
-                            <td>{rowData.date}</td>
-                            <td>{rowData.phoneNumber}</td>
-                            <td>{rowData.fixed === -1 ? "Not Fixed" : rowData.fixed === 1 ? "Fixed 1" : rowData.fixed === 2 ? "Fixed 2" : "Fixed 3"}</td>
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th onClick={() => updateSortConfig('id', sortConfig.keys.find(k => k.key === 'id')?.direction === 'ascending' ? 'descending' : 'ascending')}>
+                                ID {sortConfig.keys.find(k => k.key === 'id')?.direction === 'ascending' ? '▼' : '▲'}
+                            </th>
+                            <th onClick={() => updateSortConfig('priority', sortConfig.keys.find(k => k.key === 'priority')?.direction === 'ascending' ? 'descending' : 'ascending')}>
+                                Priority {sortConfig.keys.find(k => k.key === 'priority')?.direction === 'ascending' ? '▼' : '▲'}
+                            </th>
+                            <th> Problem Description </th>
+                            <th> Name </th>
+                            <th> Room Number </th>
+                            <th onClick={() => updateSortConfig('date', sortConfig.keys.find(k => k.key === 'date')?.direction === 'ascending' ? 'descending' : 'ascending')}>
+                                Date {sortConfig.keys.find(k => k.key === 'date')?.direction === 'ascending' ? '▼' : '▲'}
+                            </th>
+                            <th> Phone Number </th>
+                            <th onClick={() => updateSortConfig('fixed', sortConfig.keys.find(k => k.key === 'fixed')?.direction === 'ascending' ? 'descending' : 'ascending')}>
+                                Fixed {sortConfig.keys.find(k => k.key === 'fixed')?.direction === 'ascending' ? '▼' : '▲'}
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div >
+                    </thead>
+                    <tbody>
+                        {sortedTableData.map((rowData, index) => (
+                            <tr key={index}>
+                                <td>{rowData.id}</td>
+                                <td>{rowData.priority}</td>
+                                <td>{rowData.problemDescription}</td>
+                                <td>{rowData.name}</td>
+                                <td>{rowData.roomNumber}</td>
+                                <td>{rowData.date}</td>
+                                <td>{rowData.phoneNumber}</td>
+                                <td>{rowData.fixed === -1 ? "Not Fixed" : rowData.fixed === 1 ? "Fixed 1" : rowData.fixed === 2 ? "Fixed 2" : "Fixed 3"}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         );
     }
+
 
     return (
         <div className="dash">
--- app/pages/dashboard/reports/report.tsx
+++ app/pages/dashboard/reports/report.tsx
@@ -1,22 +1,47 @@
-"use client"
+"use client";
 
+import { getAllDocuments } from "@/app/api/firebase";
 import React, { useEffect, useState } from "react";
+
 export function ReportPage() {
     const [reportID, setReportID] = useState<number | null>(null);
+    const [reportData, setReportData] = useState<any>(null);
 
     useEffect(() => {
+        async function fetchData(id: number) {
+            const rawData: {
+                id: string;
+                priority?: number;
+                problemDescription?: string;
+                name?: string;
+                roomNumber?: number;
+                date?: string;
+                phoneNumber?: string;
+                completed?: boolean;
+            }[] = await getAllDocuments("dummy");
+
+            return rawData.find(data => Number.parseInt(data.id) == id);
+        }
+
         if (typeof window !== "undefined") {
             const params = new URLSearchParams(window.location.search);
             const id = params.get("reportID");
             if (id) {
-                setReportID(Number.parseInt(id));
+                const fetchReportData = async () => {
+                    const data = await fetchData(Number.parseInt(id));
+                    setReportID(Number.parseInt(id));
+                    console.log(data);
+                    setReportData(data);
+                };
+                fetchReportData();
             }
         }
     }, []);
 
     return (
         <div>
-            Report ID: {reportID}
+            <div>Report ID: {reportID}</div>
+            <div>Report Data: {JSON.stringify(reportData)}</div>
         </div>
     );
 }
