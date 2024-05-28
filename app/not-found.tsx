3380af066fef6b68faac37b3c0479f1fc0fe1ed8
OmriBerger <omriber12@gmail.com>
2024-05-28 12:34:11 +0300

    Revert "deleted not found"
    This reverts commit 5c3f88f557097ff510927755e403188379094c6e.

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function NotFound() {
+ const router = useRouter();
+
+ useEffect(() => {
+    router.push('/pages/support');
+ }, []);
+
+ return <div>Redirecting...</div>;
+}
